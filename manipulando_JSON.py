56af28cd8393b618f06803c63d029330501474e8
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2019-12-05 07:47:01 -0500

    Create manipulando_JSON.py

---
+++ manipulando_JSON.py
@@ -0,0 +1,15 @@
+import json
+
+lista = ["XXXX", "YYYY"]
+
+json_data = '{"vs":[{"XXXX":["1234", "56789", "101112"],"YYYY":["4321", "56789", "121110"]}]}' 
+
+parsed_json = (json.loads(json_data))
+
+for i in range (0, len (parsed_json["vs"])):
+	for element in lista:
+		for j in range (0, len (parsed_json["vs"][i][element])):
+			#print parsed_json["vs"][i][element][j]
+			parsed_json["vs"][i][element][j] = "0000"
+
+print(json.dumps(parsed_json, indent = 4, sort_keys=True))
